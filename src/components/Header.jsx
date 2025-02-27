3dd23a0564978e5fa1476ed98f095c6b9f5e8b11
Treasure-cd <treasureaj14@gmail.com>
2025-02-27 03:37:42 +0100

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <h1 className='logo'>GALLERY <span className='logo-red-part'>GUESS</span></h1>
+        <p className='tiny-text'>for art nerds.</p>
+        </header>
+  )
+}
